99b5ed66657c10bbe9176eb1af358a3e1c98bfd2
Gagan200 <akkugagan@gmail.com>
2021-10-14 02:55:47 +0530

    Frontend

--- server/config/google.config.js
+++ server/config/google.config.js
@@ -8,7 +8,7 @@ const GoogleStrategy = google0Auth.Strategy;
 export default (passport) => {
     passport.use(
         new GoogleStrategy({
-            clientID:process.env.GOOGLE__ID,
+            clientID:process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
             callbackURL:"http://localhost:4000/auth/google/callback"
         },
